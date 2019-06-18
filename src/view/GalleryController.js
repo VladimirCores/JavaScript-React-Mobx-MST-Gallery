bf7b9de8a45b738e443f2010492afff3f4699d26
Vladimir Minkin <Vladimir.Minkin@massive.co>
2019-06-18 20:16:07 +0200

    Mobx DevTools was removed - Chrome Extension is in use instead

--- src/view/GalleryController.js
+++ src/view/GalleryController.js
@@ -1,6 +1,5 @@
 import React, {Fragment} from 'react'
 import {observer, inject} from 'mobx-react'
-import DevTools from 'mobx-react-devtools'
 
 import Keyboard from '../consts/Keyboard'
 import DomElement from './base/DomElement'
@@ -58,7 +57,6 @@ export default class GalleryController extends DomElement {
 		<Fragment>
 			<Gallery> {this.renderThumbs()} </Gallery>
 			{this.store.lightRoomVisible && this.renderLightRoom()}
-			<DevTools />
 		</Fragment>
 
 	render() {
