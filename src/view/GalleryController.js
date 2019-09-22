92f708cbb0b43db9aac7a5dbeb28f11318c47568
Vladimir Minkin <Vladimir.Minkin@massive.co>
2019-09-22 13:30:16 +0200

    GalleryController simplification

--- src/view/GalleryController.js
+++ src/view/GalleryController.js
@@ -10,53 +10,55 @@ import Image from './gallery/Image'
 
 export default observer(['galleryStore'], class extends DomElement
 {
-    onDocumentKeyboardNavigation = (event) => {
-        event.stopImmediatePropagation()
-        event = event || window.event
-        if (event.keyCode === Keyboard.ARROW_LEFT) {
-            this.store.selectNext(-1)
-        }
-        else if (event.keyCode === Keyboard.ARROW_RIGHT) {
-            this.store.selectNext(1)
-        }
-        else if (event.keyCode === Keyboard.ENTER) {
-            this.store.toggleLightRoom()
-        }
-    }
+	onDocumentKeyboardNavigation = (event) => {
+		event.stopImmediatePropagation()
+		event = event || window.event
+		if (event.keyCode === Keyboard.ARROW_LEFT) {
+			this.store.selectNext(-1)
+		}
+		else if (event.keyCode === Keyboard.ARROW_RIGHT) {
+			this.store.selectNext(1)
+		}
+		else if (event.keyCode === Keyboard.ENTER) {
+			this.store.toggleLightRoom()
+		}
+	}
 
-    componentDidMount() {
-        document.onkeydown = this.onDocumentKeyboardNavigation
-    }
+	componentDidMount() {
+		document.onkeydown = this.onDocumentKeyboardNavigation
+	}
 
-    componentWillUnmount() {
-        document.onkeydown = null
-    }
+	componentWillUnmount() {
+		document.onkeydown = null
+	}
 
-    get selectedImage() { return this.store.selectedImageVO }
+	get selectedImage() { return this.store.selectedImageVO }
 
-    renderLightRoom = () =>
-        <LightRoom title={this.selectedImage.title}>
-            <Image width={this.selectedImage.width}
-                   height={this.selectedImage.height}
-                   url={this.store.selectedImageUrl}
-            />
-        </LightRoom>
+	renderLightRoom = () =>
+		<LightRoom title={this.selectedImage.title}>
+			<Image width={this.selectedImage.width}
+			       height={this.selectedImage.height}
+			       url={this.store.selectedImageUrl}
+			/>
+		</LightRoom>
 
-    renderThumbs = () => this.store.images.map((imageVO, index) => {
-        let thumbVO = imageVO.thumb
-        return <Thumb key={index}
-          selected={this.store.isSelected(index)}
-          width={thumbVO.width}
-          height={thumbVO.height}
-          url={thumbVO.path + thumbVO.name}
-        />
-    })
+	renderThumbs = () => this.store.images.map((imageVO, index) => {
+		let thumbVO = imageVO.thumb
+    let thumbUrl = thumbVO.path + thumbVO.name
 
-    renderGallery = () =>
-        <Fragment>
-            <Gallery> { this.renderThumbs() } </Gallery>
-            { this.store.lightRoomVisible && this.renderLightRoom() }
-        </Fragment>
+		return <Thumb key={index}
+		              selected={this.store.isSelected(index)}
+		              width={thumbVO.width}
+		              height={thumbVO.height}
+		              url={thumbUrl}
+		/>
+	})
 
-    render() { return this.store.dataLoading ? <Spinner/> : this.renderGallery() }
+	renderGallery = () =>
+		<Fragment>
+			<Gallery> { this.renderThumbs() } </Gallery>
+			{ this.store.lightRoomVisible && this.renderLightRoom() }
+		</Fragment>
+
+	render() { return this.store.dataLoading ? <Spinner/> : this.renderGallery() }
 })
